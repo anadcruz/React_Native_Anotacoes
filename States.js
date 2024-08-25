2fa3dda228127dec09ef8117049f6e7b07c5c29b
anadcruz <109186269+anadcruz@users.noreply.github.com>
2024-08-25 19:55:21 -0300

    update

---
+++ States.js
@@ -0,0 +1,10 @@
+//props = estática 
+//states = mutaveis , consegue trocar o valor das variaveis
+
+constructor(props){
+    super(props);
+    this.state = {
+        nome: 'Mateus'
+    
+    };
+}
