8c3e841cbdcc9a8ac25780b6d99755664b87e813
anadcruz <109186269+anadcruz@users.noreply.github.com>
2024-09-15 18:21:08 -0300

    add

---
+++ Flex Direction x Justify Center.js
@@ -0,0 +1,19 @@
+// *1. flexDirection
+//	• Define a direção principal do layout dos filhos dentro de um contêiner.
+//	• Valores comuns:
+//		○ row: Alinha os filhos na horizontal (da esquerda para a direita).
+//		○ row-reverse: Alinha os filhos na horizontal, mas na direção inversa (da direita para a esquerda).
+//		○ column: Alinha os filhos na vertical (de cima para baixo). Este é o valor padrão no React Native.
+//		○ column-reverse: Alinha os filhos na vertical, mas na direção inversa (de baixo para cima).
+// 2. justifyContent
+//	• Define como os filhos serão distribuídos ao longo do eixo principal (determinado por flexDirection).
+//	• Valores comuns:
+//		○ flex-start: Alinha os itens no início do eixo principal.
+//		○ flex-end: Alinha os itens no final do eixo principal.
+//		○ center: Centraliza os itens ao longo do eixo principal.
+//		○ space-between: Distribui os itens com o maior espaço possível entre eles.
+//		○ space-around: Distribui os itens com espaçamento igual ao redor de cada item.
+// Diferença Principal:
+//	• flexDirection define a direção do eixo principal (horizontal ou vertical).
+//	• justifyContent define a distribuição dos itens ao longo do eixo principal (com base no flexDirection).
+// Por exemplo, se o flexDirection for row, o justifyContent afetará como os itens são alinhados horizontalmente. Se o flexDirection for column, o justifyContent afetará o alinhamento vertical.
